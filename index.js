f616577e6a95ef5c820cecda123f6429ad25c988
Ahmd-Tint <inttni222@gmail.com>
2026-07-23 17:38:42 +0000

    Updated bot code and added express server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is awake!');
+});
+
+app.listen(port, () => {
+  console.log(`Web server running on port ${port}`);
+});
+
 require('dotenv').config();
 const { 
   Client, 
